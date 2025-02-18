41dd0c59818c582ef2060097dfb3a31c17d68e13
younes <younes3021.us@gmail.com>
2025-02-18 21:02:35 +0330

    post method added

--- wotracker/plans/views.py
+++ wotracker/plans/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from .models import PlansModel
 from .serializers import PlansDetailSerializers
 from rest_framework.response import Response
+from rest_framework import status
 
 
 
@@ -11,4 +12,11 @@ class PlansDetailView(APIView):
         plans = PlansModel.objects.all()
         serializer = PlansDetailSerializers(plans, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = PlansDetailSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
