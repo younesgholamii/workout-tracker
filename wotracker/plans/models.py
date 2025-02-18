dd4c9ddbd93f029726df6de8a1a80bf7690cf201
younes <younes3021.us@gmail.com>
2025-02-18 20:32:21 +0330

    PlansModel added

--- wotracker/plans/models.py
+++ wotracker/plans/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class PlansModel(models.Model):
+    category_choices = [
+        ('cardio', 'Cardio'),
+        ('strength', 'Strength'),
+        ('flexibility', 'Flexibility')
+    ]
+
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=50 ,choices=category_choices, default='cardio')
+    date = models.DateField()
+    description = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
